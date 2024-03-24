7b5b46ac764d53558446d6c9c4f91717e92170b5
Kovac <Dejan.Kovac@avl.com>
2024-03-24 11:09:49 +0100

    list files in directory

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from src.MDFMeasurement import MDFMeasurement
 from src.SQLDatabase import SQLDatabase
 
@@ -9,27 +10,15 @@ print('Read data from .mf4 format')
 
 def main():
 
-    file1 = 'C:/Users/u22k86/project_pte/data/Fzg01_40402_1.mf4'
-    file11 = 'C:/Users/u22k86/project_pte/data/Fzg01_40403_1.mf4'
+    folder = 'data'
+    files = []
 
-    file2 = 'C:/Users/u22k86/project_pte/data/Fzg02_40402_1.mf4'
-    file21 = 'C:/Users/u22k86/project_pte/data/Fzg02_40402_2.mf4'
-    file22 = 'C:/Users/u22k86/project_pte/data/Fzg02_40402_3.mf4'
-    file23 = 'C:/Users/u22k86/project_pte/data/Fzg02_40403_1.mf4'
-    file24 = 'C:/Users/u22k86/project_pte/data/Fzg02_40403_2.mf4'
-    file25 = 'C:/Users/u22k86/project_pte/data/Fzg02_40403_3.mf4'
+    for dirpath, dirnames, filenames in os.walk(folder):
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            file = os.path.abspath(filepath)
+            files.append(file)
 
-    file3 = 'C:/Users/u22k86/project_pte/data/Fzg03_40402_1.mf4'
-    file31 = 'C:/Users/u22k86/project_pte/data/Fzg03_40402_2.mf4'
-    file32 = 'C:/Users/u22k86/project_pte/data/Fzg03_40402_3.mf4'
-    file33 = 'C:/Users/u22k86/project_pte/data/Fzg03_40403_1.mf4'
-    file34 = 'C:/Users/u22k86/project_pte/data/Fzg03_40403_2.mf4'
-    file35 = 'C:/Users/u22k86/project_pte/data/Fzg03_40403_3.mf4'
-
-    # files = [file1, file2, file3]
-    files1 = [file1, file11, file2, file21, file22, file23, file24, file25, file3, file31, file32, file33, file34, file35]
-    files = files1
-    
     for file in files:
         mdfm = MDFMeasurement(file)
         data = mdfm.set_data()
